ce8394423c5fbd2a4dfa49e0ac1d1484949f5634
Pallav Marwaha <pallav.marwah@gmail.com>
2022-12-22 16:55:16 +0530

    add user dashboard

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,6 @@ app_name = "users"
 urlpatterns = [
     path("login/", views.user_login, name="user-login"),
     path("signup/", views.user_signup, name="user-signup"),
+    path("logout", views.user_logout, name="user-logout"),
+    path("dashboard/", views.user_dashboard, name="user-dashboard"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from .forms import UserLoginForm, UserSignUpForm
 from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 User = get_user_model()
 # Create your views here.
@@ -22,6 +25,7 @@ def user_login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request=request, user=user)
+                return render(request, "users/dashboard.html")
 
     return render(request, "users/login.html")
 
@@ -45,5 +49,15 @@ def user_signup(request):
     return render(request, "users/signup.html")
 
 
+@login_required(login_url="users:user-login")
 def user_logout(request):
     logout(request=request)
+    return HttpResponseRedirect(reverse("home-page"))
+
+
+@login_required(login_url="users:user-login")
+def user_dashboard(request):
+    """
+    Renders user dashboard
+    """
+    return render(request, "users/dashboard.html")
